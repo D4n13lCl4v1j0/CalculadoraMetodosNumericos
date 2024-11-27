203a475fdbf4c8d4f8ee98a98addf11cf80f8866
FelipePenagosss <93340395+FelipePenagosss@users.noreply.github.com>
2024-11-27 15:13:34 -0500

    CAMBIOS PARA INTERFAZ CON ANGULAR

---
+++ interfazAngular/src/app/components/simpson/simpson.component.ts
@@ -0,0 +1,83 @@
+import { Component,OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Simpson } from 'src/app/models/Simpson';
+import {SimpsonService} from 'src/app/services/simpsonService';
+
+@Component({
+  selector: 'app-simpson',
+  templateUrl: './simpson.component.html',
+  styleUrls: ['./simpson.component.css']
+})
+export class SimpsonComponent implements OnInit{
+  eventForm: FormGroup;
+  titulo = 'Método de Simpson';
+  lista: any[] = [];
+  raiz: number = 0;
+  imagen: string = '';
+  selectedField: string = ''; // Campo seleccionado para actualizar
+
+  constructor(
+    private fb: FormBuilder,
+    private simpsonS:SimpsonService,
+  ) {
+    this.eventForm = this.fb.group({
+      funcion: ['', Validators.required],
+      limitea:['',Validators.required],
+      limiteb: ['', Validators.required],
+      nimagenes: ['', Validators.required],
+    });
+  }
+  ngOnInit():void{}
+  consulta() {
+    const simpson: Simpson = {
+      funcion: this.eventForm.get('funcion')?.value,
+      limitea:this.eventForm.get('limitea')?.value,
+      limiteb: this.eventForm.get('limiteb')?.value,
+      nimagenes:this.eventForm.get('nimagenes')?.value
+    };
+    this.simpsonS.save(simpson).subscribe(
+      response=>{
+        console.log(response);
+        const data=response.Resultados;
+        this.raiz=response.Raiz;
+        this.imagen = 'data:image/png;base64,' + response.Grafica;
+        if (Array.isArray(data)) {
+          this.lista = data;
+          for (const iterator of this.lista) {
+            console.log(iterator.iteracion);
+          }
+        } else {
+          console.error('Los datos recibidos no son un array:', data);
+        }
+        
+      },
+      error => {
+        console.log(error);
+        this.eventForm.reset();
+      }
+    );
+  }
+
+  openCalculator(field: string) {
+    this.selectedField = field; // Guarda el campo seleccionado
+    const calculatorModal = document.getElementById('calculatorModal');
+    if (calculatorModal) {
+      calculatorModal.style.display = 'block';
+    }
+  }
+
+  closeCalculator() {
+    const calculatorModal = document.getElementById('calculatorModal');
+    if (calculatorModal) {
+      calculatorModal.style.display = 'none';
+    }
+  }
+
+  saveFunction(func: string) {
+    if (this.selectedField) {
+      this.eventForm.get(this.selectedField)?.setValue(func);
+      this.closeCalculator();
+    }
+  }
+
+}
