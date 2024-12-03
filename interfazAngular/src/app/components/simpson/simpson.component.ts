9692ae40ac83da48b5480ac890afa4ad2076606d
FelipePenagosss <93340395+FelipePenagosss@users.noreply.github.com>
2024-12-03 17:17:31 -0500

    validaciones simpson

--- interfazAngular/src/app/components/simpson/simpson.component.ts
+++ interfazAngular/src/app/components/simpson/simpson.component.ts
@@ -1,14 +1,14 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Simpson } from 'src/app/models/Simpson';
-import {SimpsonService} from 'src/app/services/simpsonService';
+import { SimpsonService } from 'src/app/services/simpsonService';
 
 @Component({
   selector: 'app-simpson',
   templateUrl: './simpson.component.html',
   styleUrls: ['./simpson.component.css']
 })
-export class SimpsonComponent implements OnInit{
+export class SimpsonComponent implements OnInit {
   eventForm: FormGroup;
   titulo = 'Método de Simpson';
   lista: any[] = [];
@@ -18,29 +18,45 @@ export class SimpsonComponent implements OnInit{
 
   constructor(
     private fb: FormBuilder,
-    private simpsonS:SimpsonService,
+    private simpsonS: SimpsonService,
   ) {
     this.eventForm = this.fb.group({
       funcion: ['', Validators.required],
-      limitea:['',Validators.required],
+      limitea: ['', Validators.required],
       limiteb: ['', Validators.required],
-      nimagenes: ['', Validators.required],
+      nimagenes: ['', [Validators.required, Validators.min(2), this.parValidator]],
     });
   }
-  ngOnInit():void{}
+
+  ngOnInit(): void {}
+
+  // Validador personalizado para el campo nimagenes (verifica que sea par)
+  parValidator(control: any) {
+    if (control.value && control.value % 2 !== 0) {
+      return { parError: 'El número de imágenes debe ser par' };
+    }
+    return null;
+  }
+
   consulta() {
+    if (this.eventForm.invalid) {
+      return; // Si el formulario es inválido, no hacer nada
+    }
+
     const simpson: Simpson = {
       funcion: this.eventForm.get('funcion')?.value,
-      limitea:this.eventForm.get('limitea')?.value,
+      limitea: this.eventForm.get('limitea')?.value,
       limiteb: this.eventForm.get('limiteb')?.value,
-      nimagenes:this.eventForm.get('nimagenes')?.value
+      nimagenes: this.eventForm.get('nimagenes')?.value
     };
+
     this.simpsonS.save(simpson).subscribe(
-      response=>{
+      response => {
         console.log(response);
-        const data=response.Resultados;
-        this.raiz=response.Raiz;
+        const data = response.Resultados;
+        this.raiz = response.Raiz;
         this.imagen = 'data:image/png;base64,' + response.Grafica;
+
         if (Array.isArray(data)) {
           this.lista = data;
           for (const iterator of this.lista) {
@@ -49,7 +65,6 @@ export class SimpsonComponent implements OnInit{
         } else {
           console.error('Los datos recibidos no son un array:', data);
         }
-        
       },
       error => {
         console.log(error);
